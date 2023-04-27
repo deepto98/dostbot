36de58a1a309c9acfed8bd3307d5da9e4ce3bb35
deepto <ddepp98@outlook.com>
2023-04-28 02:39:49 +0530

    reminder works with tag for sender

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -16,36 +16,28 @@ const agenda = new Agenda({ db: { address: dbUri } });
  * @param {Message} message The string
  */
 agenda.define("send_reminder", async (job) => {
-  var { chatId,messageId } = job.attrs.data;
-//   console.log("agenda msg");
-//   console.log(message);
-//   var reconMessage = Object.assign(new Message(), message);
-//   console.log("recon msg");
-//   console.log(reconMessage);
-  //   reconMessage.reply("Reminder");
-
-  client.sendMessage(chatId, "Reminder");
-//   message.reply("Reminder");
+  var { chatId, messageId, senderId, senderName } = job.attrs.data;
+  var mentions = [];
+  await client.getContactById(senderId).then((contact) => {
+    mentions.push(contact);
+    var text = `@${senderName} `;
+    client.sendMessage(chatId, text, { mentions });
+  });
 });
 
 /**
  * @param {Message} message The string
  */
 async function createReminder(message) {
-  console.log("org msg");
-  // IIFE to give access to async/
-
   var chatId = (await message.getChat()).id._serialized;
   var messageId = message.id.id;
-//   console.log(message);
-  console.log(chatId);
-  console.log(messageId);
 
-    await agenda.start();
+  await agenda.start();
 
-  // await agenda.every('3 minutes', 'delete old users');
-    agenda.schedule("in 20 seconds", "send_reminder", {
-      chatId: chatId,
-      messageId: messageId,
-    });
+  await agenda.schedule("in 10 seconds", "send_reminder", {
+    chatId: chatId,
+    messageId: messageId,
+    senderId: message.author,
+    senderName: (await message.getContact()).id.user,
+  });
 }
